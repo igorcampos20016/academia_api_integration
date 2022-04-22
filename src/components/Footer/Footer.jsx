26e036883a52a319f3e9593281fb461319df94f6
igorcampos20016 <igorcampos20016@gmail.com>
2022-04-22 16:53:12 -0300

    subindo Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Footer.module.css";
+function Footer() {
+  return (
+    <div>
+      <footer className={styles.footerEstilo}>Resilia-Academy</footer>
+    </div>
+  );
+}
+
+export default Footer;
