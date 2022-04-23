1d70adb6e75a356f906c14bfa349004c465320af
Jhack Costa <jhacktibia@gmail.com>
2022-04-23 20:38:31 -0300

    pagina de login estatica, ainda não está responsiva

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Button} from "../styled-components/button/button.jsx";
 import {Input} from "../styled-components/input/input.jsx";
 import {Label} from "../styled-components/label/label.jsx"
+import styles from "./login.module.css";
 
 
 function Login() {
@@ -9,14 +10,39 @@ function Login() {
     return(
 
 		
-		<div>
-				<Label>
-					<p>Email: </p>
-					<Input/>
-					<p>Senha: </p>
-					<Input/>
-					<Button>Salvar</Button>
-				</Label>
+		<div className={styles.principal}>
+
+			
+			<div className={styles.propagada}>
+
+			<img className={styles.imagem} src="https://i.pinimg.com/originals/ae/bb/95/aebb950866dfc6c867f80a80cc27f370.jpg" alt="Casal Saúdavel"/>		
+
+			</div>
+
+			<form className={styles.form}>
+				<div className={styles.home}>
+					<Label >
+						<div className={styles.corpo}>
+							<p>Digite seu e-mail: </p>
+							<Input type="email" className={styles.input} placeholder = "E-mail"/>
+							<p >Digite sua senha: </p>
+							<Input type="password" className={styles.input} placeholder = "Password"/>
+						</div>
+						<div className={styles.rodaPe}>
+							<Button type="submit" className={styles.botao}>Entrar</Button>
+							<Button type="submit" className={styles.botao}>Esqueci Senha</Button>
+							<Button type="submit" className={styles.botao}>Criar conta</Button>
+						</div>			
+					</Label>
+				</div>
+			</form>
+
+			<div className={styles.propagada}>
+
+			<img className={styles.imagem} src="https://i.pinimg.com/originals/ae/bb/95/aebb950866dfc6c867f80a80cc27f370.jpg" alt="Casal Saúdavel"/>		
+			
+			</div>
+
 		</div>
 					
 
