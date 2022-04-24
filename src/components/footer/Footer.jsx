8fe1bac380976cae9d05b79c5d88e34a00666b9d
igorcampos20016 <igorcampos20016@gmail.com>
2022-04-24 17:14:40 -0300

    Subindo o Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Footer.module.css";
+function Footer() {
+  return (
+    <div>
+      <footer className={styles.footerEstilo}>Esse é o Footer da T11 🦈</footer>
+    </div>
+  );
+}
+
+export default Footer;
